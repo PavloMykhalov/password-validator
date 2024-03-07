d305f7bbe8d75074bc70c745b6b6b927cd4e7165
Pavlo Mykhalov <pavlomihaliov@gmail.com>
2024-03-07 12:47:15 +0200

    add fix for favicon

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,7 +12,6 @@ import { RouterOutlet } from '@angular/router';
     CommonModule
   ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
 })
 export class AppComponent {
   password: string = '';
